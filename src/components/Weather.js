de7501b80ec0486940e87aba041e96b8a4d82a50
ant1991 <grishanya-91@mail.ru>
2019-10-17 16:17:02 +0300

    js

---
+++ src/components/Weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Weather = props => (
+    <div>
+        { props.city && 
+          <div>
+            <p>Местоположение:{props.city}, {props.country}</p>
+            <p>Температура: {props.temp}</p>
+            <p>Давление: {props.pressure}</p>
+            <p>Заход солнца: {props.sunset}</p>
+          </div>
+        }
+        <p>{ props.error }</p>
+        </div>
+);
+
+export default Weather;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
